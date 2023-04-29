ebceab714dc21d5283c1b5c8b988e3524070cd7e
youngsinatray <ohzhink@gmail.com>
2023-04-29 19:22:14 +0200

    [~] Changed schedule, second try

--- carga_docker.bat
+++ carga_docker.bat
@@ -2,7 +2,7 @@ docker build youngsinatra/hoysi .
 doucker push hoysi:latest
 
 git config --global user.name youngsinatray
-git config --global user.email youngsinatra@gmail.com
+git config --global user.email ohzhink@gmail.com
 
 git add .
 git commit -m "[~] Changed schedule, second try"
