778dcff749518331e1a420f2ebd1523e12573bba
youngsinatray <ohzhink@gmail.com>
2023-05-06 20:35:22 +0200

    [~] Changed book time

--- carga_docker.bat
+++ carga_docker.bat
@@ -1,3 +1,5 @@
+@REM git config --global user.name youngsinatray
+@REM git config --global user.email ohzhink@gmail.com
 
 @REM Uploading docker image to DockerHub  
 docker build -t youngsinatra/hoysi . 
@@ -5,8 +7,5 @@ docker push youngsinatra/hoysi:latest
 
 @REM Pushing to repo chages (the ones in .github/ are the ones that matters)
 git add .
-git commit -m "[~] Changed schedule, second try"
+git commit -m "[~] Changed book time"
 git push
-
-@REM git config --global user.name youngsinatray
-@REM git config --global user.email ohzhink@gmail.com
--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ def run_at_specific_time(client: AimHarderClient, target_day: str, class_id1, cl
     if (int(target_time.strftime("%H")) - int(datetime.now().strftime("%H"))) > 60:
         raise Exception("Más de 60 minutos de diferencia", target_time.strftime("%H:%M:%S"))
     
-    wait_time = (target_time - now).total_seconds() + 60
+    wait_time = (target_time - now).total_seconds() + 62
     print("Target time: " + str(target_time),"now: " + str(now),"Diferencia en segundos: "+ str(wait_time))
     print("Restamos: 60 - " + str(now.minute),"= " +str(60 - now.minute), "Diferencia en horas: "+ str(wait_time / 3600))
     if wait_time < 0:
